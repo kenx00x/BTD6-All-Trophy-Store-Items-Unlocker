8476997a913a2f402adf79cc46347c6b81910ad4
kenx00x <kenx00x@gmail.com>
2021-04-16 12:51:12 +0200

    fixed for update 25+using melonloader 0.3

--- BTD6 All Trophy Store Items Unlocker/Class1.cs
+++ BTD6 All Trophy Store Items Unlocker/Class1.cs
@@ -1,9 +1,10 @@
-﻿using Assets.Scripts.Unity;
+﻿using Assets.Scripts.Data;
+using Assets.Scripts.Unity;
 using Assets.Scripts.Unity.UI_New.Main;
 using Harmony;
 using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-[assembly: MelonInfo(typeof(BTD6_All_Trophy_Store_Items_Unlocker.Class1), "All Trophy Store Items Unlocker", "1.0.1", "kenx00x")]
+[assembly: MelonInfo(typeof(BTD6_All_Trophy_Store_Items_Unlocker.Class1), "All Trophy Store Items Unlocker", "1.1.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_All_Trophy_Store_Items_Unlocker
 {
@@ -11,7 +12,7 @@ namespace BTD6_All_Trophy_Store_Items_Unlocker
     {
         public override void OnApplicationStart()
         {
-            MelonLogger.Log("All Trophy Store Items Unlocker loaded!");
+            MelonLogger.Msg("All Trophy Store Items Unlocker loaded!");
         }
         [HarmonyPatch(typeof(MainMenu), "Open")]
         public class TitleScreen_Patch
@@ -24,12 +25,12 @@ namespace BTD6_All_Trophy_Store_Items_Unlocker
                 {
                     testList.Add(item.Key);
                 }
-                foreach (var item in Game.instance.trophyStoreItems.storeItems)
+                foreach (var item in GameData._instance.trophyStoreItems.storeItems)
                 {
                     if (!testList.Contains(item.id))
                     {
                         Game.instance.playerService.Player.AddTrophyStoreItem(item.id);
-                        MelonLogger.Log($"Unlocked {item.id}");
+                        MelonLogger.Msg($"Unlocked {item.id}");
                     }
                 }
             }
